c9fd8fa6b8490054b0e039c6f08fd45af17788a1
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-01-23 01:23:51 -0800

    Root WorldEditor Removal (oops)
    Not sure where I messed this part up, meant to remove this back in 7b17481. I think I got distracted with writing the message, and I accidentally removed this file from staging (or rather, ran `git checkout .` without seeing that this had something there).

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import FileManipulator from "./FileManipulator.tsx";
-import WorldEditor from "./WorldEditor.tsx";
 import "./App.css";
 
 export default function App() {
   return (
-    <>
-      <FileManipulator />
-      <WorldEditor />
-    </>
+    <FileManipulator />
   );
 }
